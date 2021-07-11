6fc9a1316ee53f97cc441fabbe9fcb2656d24518
reina <buizark@gmail.com>
2021-07-11 12:45:33 -0700

    cache modloader

--- mod.gd
+++ mod.gd
@@ -1,4 +1,7 @@
 extends Reference
 
+var modloader: Reference
+
 func load(modloader: Reference, mod_info, tree: SceneTree):
+    self.modloader = mod_info
     print(mod_info.name + " is loading!")
